cf9b96022860200a296482fed7bc8a5e4318f307
cathyJenny <lyu14@u.rochester.edu>
2014-11-11 17:03:51 -0500

    Update and rename login.py to LOGIN.py

--- LOGIN.py
+++ LOGIN.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
-import cgi
-import cgitb
 
+import cgi
+# to facilitate debugging
+import cgitb
 cgitb.enable()
+
+#connect database
 import sqlite3
 conn = sqlite3.connect('/home2/jmaosixr/users.db')
 c = conn.cursor()
+
+#Cookie
 import Cookie
 import os
 exist_cookie = os.environ.get('HTTP_COOKIE')
 
-
-# check if cookie exists
 if exist_cookie:
 	coo = Cookie.SimpleCookie(exist_cookie)
 	session_ID = coo['session'].value
@@ -20,14 +23,9 @@ if exist_cookie:
 	if len(all_results) > 0:
 		back_username = all_results[0][0]
 		print "Content-type: text/html"
+		# don't forget the extra newline!
 		print
-		print "<html>"
-		print "<body>"
-		print "<h1>Welcome again, " + back_username + "</h1>"
-		print "<form action='logout.py'><input type='submit' value='logout'/></form>"
-		print "</body>"
-		print "</html>"
-		
+		print "<h1>Welcome again!!! "+back_username+"</h1>"
 	else:
 		print "Content-type: text/html"
 		print
@@ -36,22 +34,8 @@ if exist_cookie:
 		print "<h1>you already logged out.</h1>"
 		print "</body>"
 		print "</html>"
-#cookie does not exist
-else:
-	 print "Content-type: text/html"
-	 print
-	 print """ 
-	 <html> 
-	 <head>
-	 <title> Login </title>
-	 </head>
-	 <body> 
-
-	 <form method="POST" action="log.py"> 
-	 <h1> Enter your info to log in:</h1>
-	 Email: <input name="email" type=text size=30/><br></br>
-	 Password:<input name="pw" type="password" size=30/><br></br>
-	 <input type="submit"/><input type="reset"/>
-	 </form>
-	 </body>
-	 </html> """
+else :	
+	print "Content-type: text/html"
+	# don't forget the extra newline!
+	print
+	print "Cookie"
